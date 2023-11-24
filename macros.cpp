c790bf79c17d0e4ec86cde59afba27f4e0931bd8
Hanz Po <hanznathanpo@gmail.com>
2023-11-24 09:48:27 -0500

    More files

---
+++ macros.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+#define SIX 6
+
+int main() {
+  cout << SIX + SIX;
+  return 0;
+}
---
+++ number_word.cpp
@@ -0,0 +1,7 @@
+// Use a map to map a number to the word (e.g. 1 -> "one")
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
